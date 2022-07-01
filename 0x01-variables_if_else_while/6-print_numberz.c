7d025ed69f0fe5099875235c8204717474a94deb
Oluwadare <oluwadareadeneye@gmail.com>
2022-07-01 05:24:37 -0700

    print 0 to 9 using puchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -  prints all single digit numbers of base 10 starting from 0
+ * only using putchar and without char variables.
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n;
+
+for (n = 48; n <= 57; n++)
+putchar(n);
+putchar(10);
+return (0);
+}
