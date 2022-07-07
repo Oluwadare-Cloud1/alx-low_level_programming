a95475a1999103dba5b527034f62e8c9e10c78b4
Oluwadare <oluwadareadeneye@gmail.com>
2022-07-07 06:26:06 -0700

    checks for upper case alphas

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper -> this is a function to check for upper case alphabet
+ * @c: an integer parameter
+ * Return: something but success
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+return (0);
