cc120518dadb3bec14dc6fe160d6c45264c4882c
Oluwadare <oluwadareadeneye@gmail.com>
2022-08-18 06:09:55 -0700

    fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,69 +1,46 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-
 /**
- * _strlen - find the length of a string
- * @s: pointer to the string to check
- * Return: void
-*/
-
-
-int _strlen(char *s)
-{
-int i = 0;
-while (s[i])
-	i++;
-
-return (i);
-}
-
-
-/**
- * _pow_recursion - Search a string for any of a set of bytes.
- * @x: base
- * @y: exposant
- * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
- * or NULL if no such byte is found.
- */
-
-int _pow_recursion(int x, int y)
-{
-
-if (y < 0)
-	return (-1);
-else if (y == 1)
-	return (x);
-else if (y == 0)
-	return (1);
-
-return (x * _pow_recursion(x, y - 1));
-
-}
-
-
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: binary number
+ * binary_to_uint - converts a binary to an unsigned int
+ * @b: the binary number as a string
  *
- * Return: 0 or converted number
+ * Return: the converted value
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
-	int i, len;
+unsigned int decimal = 0;
+int str_len = 0, base = 1;
 
-	if (b == NULL)
-		return (0);
-	len = _strlen(b);
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] == '1')
-			n += _pow_recursion(2, (len - i - 1));
-		else if (b[i] != '0')
-			return (0);
-	}
+if (!check_valid_string(b))
+return (0);
 
-	return (n);
+while (b[str_len] != '\0')
+str_len++;
+
+while (str_len)
+{
+decimal += ((b[str_len - 1] - '0') * base);
+base *= 2;
+str_len--;
+}
+return (decimal);
+}
+
+/**
+ * check_valid_string - checks if a string has only 0's and 1's
+ * @b: string to be checked
+ *
+ * Return: 1 if string is valid, 0 otherwise
+ */
+int check_valid_string(const char *b)
+{
+if (b == NULL)
+return (0);
+
+while (*b)
+{
+if (*b != '1' && *b != '0')
+return (0);
+b++;
+}
+return (1);
 }
